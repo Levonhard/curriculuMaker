bae92588be0318221682d0e24bf3d9bb00c848ec
Leonardo Rodrigues <levonhard7@gmail.com>
2020-04-17 09:07:25 -0300

    Login page fully functional

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,5 +1,9 @@
 import React, { useState } from 'react'
+import { render } from "react-dom"
 import { Link, useHistory } from 'react-router-dom'
+import { FiLogIn } from 'react-icons/fi'
+import Alert from 'react-bootstrap/Alert'
+import { CSSTransition } from 'react-transition-group'
 
 import api from '../../services/api'
 
@@ -10,6 +14,22 @@ export default function Logon() {
     const [pswd, setPswd] = useState('')
     const history = useHistory()
 
+    const LoginErrorElement = function() {
+        const [show, setShow] = useState(true);
+
+        if (show) {
+            return (
+                <CSSTransition in={show} appear={show} timeout={300} classNames="alert">
+                    <Alert variant="danger" onClose={() => setShow(false)} dismissible>
+                        <p>
+                            Email ou senha incorretos.<br></br>Tente novamente.
+                        </p>
+                    </Alert>
+                </CSSTransition>
+            )
+        }
+    }
+
     async function handleLogin(e) {
         e.preventDefault()
 
@@ -18,15 +38,24 @@ export default function Logon() {
             pswd,
         }
 
-        const response = await api.post('sessions', data)
+        try {
+            const response = await api.post('sessions', data)
 
-        console.log(response.data.name)
+            localStorage.setItem('userId', response.data.id)
+            localStorage.setItem('userName', response.data.name)
+            localStorage.setItem('userUserName', response.data.username)
+
+            history.push('/profile')
+        } catch {
+            render(<LoginErrorElement />, document.getElementById('login-error'))
+        }
     }
 
     return (
         <div className="logon-container">
-            <section className="form">
+            <div id="login-error"></div>
 
+            <section className="form">
                 <form onSubmit={handleLogin}>
                     <h1>Login</h1>
 
@@ -46,7 +75,8 @@ export default function Logon() {
                     />
                     <button className="button" type="submit">Entrar</button>
 
-                    <Link>
+                    <Link className="back-link" to="/register">
+                        <FiLogIn size={16} color="#E02041" />
                         Não tenho cadastro
                     </Link>
                 </form>
